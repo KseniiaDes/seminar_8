d5c596e88e38f595246ee4e4631290a845c62515
Kseniia Des <kseniya250496@mail.ru>
2022-12-11 12:11:13 +0300

    Add Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами 
+// первую и последнюю строку массива.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 4
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("  |");
+    }
+}
+
+void ReplaceLines(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(6, 4, -10, 10);
+PrintMatrix(array2D);
+Console.WriteLine();
+ReplaceLines(array2D);
+PrintMatrix(array2D);
